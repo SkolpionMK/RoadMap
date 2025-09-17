0192134aecd7e232e7e684c310fdceb3d21e540c
SkolpionMK <felipe.ferreira5@estudante.ifms.edu.br>
2025-09-16 21:07:58 -0400

    Class Node created

---
+++ Classes/nodeclass.js
@@ -0,0 +1,18 @@
+export default class Node {
+    constructor(name, description, duration) {
+        this.id == null //null while the Database isn't ready yet
+        this.name = name
+        this.description = description
+        this.duration = duration
+        this.links = []
+    }
+
+    create_link(node_id) {
+        try {
+            this.links.push(node_id)
+            return "Link Created Succefully!"
+        } catch(err) {
+            return `Something Went Wrong: ${err}`
+        }
+    }
+}
